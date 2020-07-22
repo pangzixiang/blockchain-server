f33ad291399c883009c1b95205ed02fdea22665e
pangzixiang <65987771+pangzixiang@users.noreply.github.com>
2020-07-22 21:27:55 +0800

    Initial commit

---
+++ src/main/java/com/scutgroup3/blockchainserver/BlockchainServerApplication.java
@@ -0,0 +1,13 @@
+package com.scutgroup3.blockchainserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockchainServerApplication.class, args);
+    }
+
+}
